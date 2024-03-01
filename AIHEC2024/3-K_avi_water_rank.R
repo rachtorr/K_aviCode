6c4a50adb8cb921a11ee6a4e7a3668065b7bbb39
Rachel D Torres <rdt47@humboldt.edu>
2024-02-29 16:41:10 -0800

    edited scripts 3 and 4

--- AIHEC2024/3-K_avi_water_rank.R
+++ AIHEC2024/3-K_avi_water_rank.R
@@ -3,9 +3,15 @@
 ## 2 February 2022
 ## Modified by Cazimir Kowalski
 ## 27 July 2022
+## Modified by Rachel Torres 22 Feb 2024 
 
 #### set up work space ####
 # clear global environment
+rm(list=ls())
+
+# this activity builds on the previous ones by loading in the same data set and creating a check for water quality standards
+# familiar functions used include: mutate(), ggplot(), group_by() and summarise(), inner_join() 
+# new functions learned include: ifelse(), 
 
 # load necessary libraries
 library(tidyverse)
@@ -15,21 +21,37 @@ tek_sum <- read.csv("tek_summary.csv")
 
 #### Rate the water quality data ####
 
+# the next part uses the function if_else, which follows this pattern: 
+# if_else(Conditional, if True then return this value, if false then return this value)
+?if_else
+
 # pipe the water quality data
-water <- water %>%
+water_check <- water %>%
   # use the mutate function to create new columns (one for each water quality metric),
   # that checks if the metric for each measurement date pases or fails based on the
   # K'avi water quality standards, where 1 = pass, 0 = fail
-  mutate(temp_check = if_else(water_temp_C <= 15, 1, 0), # should it be less than OR less than and equal to
+  mutate(temp_check = if_else(water_temp_C <= 15, 1, 0), 
          DO_check = if_else(DO >= 12, 1, 0),
-         # pH_check = if_else(pH < 7.1, 1, 0)
-         # there's no pH dataset in the data you gave me
          SC_check = if_else(specific_conductivity <= 400, 1, 0))
+
+# the following plot helps to visualize what is happening with ifelse, using water temperature as an example
+# the dashed horizontal line is out conditional, at water temp = 15 
+# above that line, water is warmer, and fails our check. the new variable "temp_check" is equal to 0
+# below the dashed line, water is normal temperatures and passes, so temp_check = 1 
+ggplot(water_check) + geom_hline(aes(yintercept=15), linetype="dashed", size=2) + 
+  geom_point(aes(x=as.Date(date), y=water_temp_C, col=as.factor(temp_check))) +
+  labs(color="temp_check", x="Date", y="Water Temp. (C)") + 
+  geom_text(aes(x=as.Date("1990-1-1"), y=22, label="Fail=0"), col="red", size=6, nudge_x = 100) +
+  geom_text(aes(x=as.Date("1990-1-1"), y=12, label="Pass=1"), col="darkgreen", size=6, nudge_x=150)
+
+# you can also use head() or summary() to see the new columns we added with mutate 
+
+
 
 # Now that we know when each standard passes / fails, we can count them to
 # figure out how often each sampling location fails based on each measurement
 # so, pipe our water quality data
-rank_sum <- water %>%
+rank_sum <- water_check %>%
   # group the data by the sampling location
   group_by(sample_location) %>%
   # summarize the data
@@ -54,28 +76,16 @@ rank_sum <- water %>%
             )
 
 ## create a new data frame that merges water quality ranking data with TEK data
-
-# monitoring site 1 pairs with ceremony site
-# monitoring site 2 pairs with medicinal foods site
-# monitoring site 3 pairs with hunting grounds
-# monitoring site 4 pairs with fishing site
-# monitoring site 5 pairs with language camp
-
-dat <- cbind(rank_sum[1,],tek_sum[1,])
-dat[2,] <- cbind(rank_sum[2,], tek_sum[5,])
-dat[3,] <- cbind(rank_sum[3,], tek_sum[3,])
-dat[4,] <- cbind(rank_sum[4,], tek_sum[2,])
-dat[5,] <- cbind(rank_sum[5,], tek_sum[4,])
-
+w_check_tek <- inner_join(rank_sum, tek_sum)
 
 ## visualize the data
-dat %>%
+w_check_tek %>%
   # plot the average failure rate for each monitoring site on x-axis against
   # average TEK ranking based on site pairings
   ggplot(aes(x = average_fail, y = mean_tek)) +
   # plot these data as points, make the point size larger so it's more visible,
   # and color by monitoring site
-  geom_point(aes(color = sample_location, size = 2.5)) +
+  geom_point(aes(color = sample_location), size = 10) +
   # add x-axis label
   xlab("Average Failure for K'avi Water Quality Standards (%)") +
   # add y-axis label
@@ -85,7 +95,15 @@ dat %>%
   # clean up plot by changing the theme
   theme_classic()
 
-# used tidyverse filter to see how many were above thresholds 
-# also introduced ifelse in this part 
-# summarised by percent of samples that failed test and plotted 
-# could ask to do different plots here 
+# what other plots could be made? 
+
+# can create other plots that visualize the different variable percent fails 
+
+
+### --- finished this activity! ---###
+
+# topics covered in this section:
+# introduced ifelse() to create new columns that show whether a variable passed a water quality standard 
+# summarised by percent of samples that failed test
+# more plotting with ggplot() 
+
--- AIHEC2024/4-Bacteria_Nutrient_Code_Integration.R
+++ AIHEC2024/4-Bacteria_Nutrient_Code_Integration.R
@@ -3,19 +3,25 @@
 ## 9 February 2022
 ## Modified by Cazimir Kowalski
 ## 27 July 2022
+## edited by Rachel Torres Feb 22 2024 
 
 #### set up work space ####
 
 # load necessary libraries
 library(tidyverse)
 library(lubridate)
+library(janitor)
 
+# This activity repeats the previous water quality activity with a new dataset on bacteria and nutrients 
+# the same steps are taken:
+# loading data, cleaning, summarising, joining, and plotting
 
+# new functions include: clean_names(), full_join() 
+# functions we've already used include: mdy(), year(), select(), group_by() and summarize_at(), mutate(), filter(), ggplot(), ifelse()  
 
 #### Part 1: Summarize nutrient and bacteria data ####
 
 ## read in data and check for missing data
-# the document needs to be a CSV, so change the file format in Excel
 bact <- read.csv("K_avi Bacteria and Nutrient Data.csv")
 
 # look at the structure of the file, see what might need to be changed
@@ -27,107 +33,99 @@ str(bact)
 # create a column that's specific to the year
 bact$year <- year(bact$date)
 
-# if you want, you can also remove the first column and change the column names,
-# using colnames function, but neither of these steps are necessary
-bact <- bact[,-1]
-colnames(bact) <- c("sample_location", "longitude", "latitude", "date",
-                     "e_coli", "tot_coliform", "nitrate", "phosphorus",
-                     "turbidity", "year")
+# if you want, you can also remove the first column using select and a minus (-)
+bact <- bact %>% select(-X)
 
+# as an option, you can also change the column names to be all lower case 
+# we previously did this using select, but let's use a new function from janitor package 
+?clean_names
+
+bact_clean <- clean_names(bact)
+head(bact_clean)
 
 ## now we can start summarizing the data, we'll look at it on an annual level
-bact_sum <- bact %>%
-  group_by(sample_location, year) %>% # you can group by multiple or a single group(s) to make this summary
-  summarize(mean_ecoli = mean(e_coli, na.rm = T),
-            median_ecoli = median(e_coli, na.rm = T),
-            min_ecoli = min(e_coli, na.rm = T),
-            max_ecoli = max(e_coli, na.rm = T),
-            mean_tot_col = mean(tot_coliform, na.rm = T),
-            median_tot_col = median(tot_coliform, na.rm = T),
-            min_tot_col = min(tot_coliform, na.rm = T),
-            max_tot_col = max(tot_coliform, na.rm = T),
-            mean_phos = mean(phosphorus, na.rm = T),
-            median_phos = median(phosphorus, na.rm = T),
-            min_phos = min(phosphorus, na.rm = T),
-            max_phos = max(phosphorus, na.rm = T),
-            mean_turb = mean(turbidity, na.rm = T),
-            median_turb = median(turbidity, na.rm = T),
-            min_turb = min(turbidity, na.rm = T),
-            max_turb = max(turbidity, na.rm = T))
+bact_sum <- bact_clean %>% 
+  group_by(sample_location, year) %>%  # we want one data point per year and sample location, so we are using group() 
+  summarize_at(vars(e_coli_col_100_m_l, total_coliform_col_100_m_l, mg_l, turbidity_ntu),
+               list(mean=mean, median=median, min=min, max=max), na.rm=TRUE)
 
+head(bact_sum)
+
+#########################################################
 #### Part 2: bind to existing water quality dataset ####
+#######################################################
 
 # read in cleaned water quality dataset
 water <- read.csv("K'avi Tribe Water Quality Dataset_clean.csv")
 
 # remove the line number (X), column
-water <- water[,-1]
+water <- water %>% select(-X)
 
 # we're going to bind the two datasets together using a function in tidyverse
 # full_join() is much more reliable & versitle function than merge(),
 # which is a base R function
-join <- full_join(water, bact)
+water_bact <- full_join(water, bact_clean)
 # but we're seeing an error, so we need to fix the date in the water df into a date, and make it not a character
 water$date <- ymd(water$date)
 # now we can try again to join them
-join <- full_join(water, bact) # success
+water_bact <- full_join(water, bact_clean) # success
 # this function is great, because it is telling us all the information it is joining by!
 
-
+##############################################
 #### Part 3: bind to TEK summary dataset ####
+############################################
 
 # read in the TEK summary dataset so we know what we're working with
 tek_sum <- read.csv("tek_summary.csv")
 
 # it can be helpful to know what our column names are when we're summarizing
 # data, so try this to have a list
-names(join)
+names(water_bact)
 
 # now we need to summarize the water quality dataset so that we can have them
 # in the same format to merge them together
-join_sum <- join %>%
+water_bact_mean <- water_bact %>%
   group_by(sample_location) %>%
   # find the mean for all of the different water quality metrics
   summarise(mean_DO = mean(DO),
             mean_temp = mean(water_temp_C),
             mean_sc = mean(specific_conductivity),
-            mean_ecoli = mean(e_coli, na.rm = T),
-            mean_tot_col = mean(tot_coliform, na.rm = T),
-            mean_nit = mean(nitrate, na.rm = T),
-            mean_phos = mean(phosphorus, na.rm = T),
-            mean_turb = mean(turbidity, na.rm = T))
-
-# create a paired dataset of monitoring site and TEK site in a new data frame
-# monitoring site 1 pairs with ceremony site
-# monitoring site 2 pairs with medicinal foods site
-# monitoring site 3 pairs with hunting grounds
-# monitoring site 4 pairs with deep water fishing site
-# monitoring site 5 pairs with language camp
-
-dat <- cbind(join_sum[1,], tek_sum[1,])
-dat[2,] <- cbind(join_sum[2,], tek_sum[5,])
-dat[3,] <- cbind(join_sum[3,], tek_sum[3,])
-dat[4,] <- cbind(join_sum[4,], tek_sum[2,])
-dat[5,] <- cbind(join_sum[5,], tek_sum[4,])
+            mean_ecoli = mean(e_coli_col_100_m_l, na.rm = T),
+            mean_tot_col = mean(total_coliform_col_100_m_l, na.rm = T),
+            mean_nit = mean(nitrates_mg_l, na.rm = T),
+            mean_phos = mean(mg_l, na.rm = T),
+            mean_turb = mean(turbidity_ntu, na.rm = T))
 
 
+tek_water_bact_mean <- inner_join(water_bact_mean, tek_sum)
+
+# how might we want to visualize this combined dataframe?
+# make a plot here with ggplot that includes: the tek rank, the monitoring site, and the mean nitrates (or pick another variable from the bacteria and nutrients data)
+
+
+  
+###############################################
 #### Part 4: Nutrient data failure calculations
+###############################################
+
+# here we can review the ifelse() function 
+# the format is ifelse(condition, value if true, value if false) 
 
 # pipe the water quality data - you can either use the bact dataframe or join
 # I am just going to work with bact to make sure sure we're working with less data
-bact <- bact %>%
+bact_check <- bact_clean %>%
   # use the mutate function to create new columns (one for each water quality metric),
   # that checks if the metric for each measurement date pases or fails based on the
   # K'avi water quality standards, where 1 = pass, 0 = fail
-  mutate(ecoli_check = if_else(e_coli >= 126, 1, 0),
-         tot_col_check = if_else(tot_coliform >= 250, 1, 0),
-         nit_check = if_else(nitrate >= 10, 1, 0),
-         phos_check = if_else(phosphorus >= 0.07, 1, 0))
+  mutate(ecoli_check = if_else(e_coli_col_100_m_l >= 126, 1, 0),
+         tot_col_check = if_else(total_coliform_col_100_m_l >= 250, 1, 0),
+         nit_check = if_else(nitrates_mg_l >= 10, 1, 0),
+         phos_check = if_else(mg_l >= 0.07, 1, 0))
 
 # Now that we know when each standard passes / fails, we can count them to
 # figure out how often each sampling location fails based on each measurement
 # so, pipe our water quality data
-rank_sum <- bact %>%
+rank_sum <- bact_check %>%
   # remove all the NA rows by filtering for only the rows that have a 1 or 0
   filter(ecoli_check == 1 | ecoli_check == 0) %>%
   # group the data by the sampling location
@@ -167,3 +165,24 @@ rank_sum %>%
   # wrap the monitoring site names so that the text doesn't overlap
   scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) +
   theme_classic()
+
+# the above plot shows us the average failure across all water standards by site, but doesn't show us which water standards are the highest percents or which monitoring sites have higher tek ranking
+## what if we want to visualize multiple variables at once? we can do this by assigning variables to shape and color 
+rank_sum %>% 
+  inner_join(tek_sum) %>% 
+  select(mean_tek, site, ecoli_percent, tot_col_percent, nit_percent, phos_percent, site) %>% 
+  pivot_longer(c(ecoli_percent, tot_col_percent, nit_percent, phos_percent)) %>% 
+  ggplot(aes(x=name, y=site, size=value, col=mean_tek)) +
+  geom_count()
+
+
+
+##############################################
+# topics covered in part 4 include:
+## dataframe manipulation: mutate, select, inner_join, pivot_longer 
+## group_by and summarize
+## another application of ifelse function 
+## joining data frames with the same columns 
+## more visualization with ggplot 
+  
+
